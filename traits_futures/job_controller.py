fbe327a15e155c61fc952a53d305aeb6dcbde09e
Mark Dickinson <mdickinson@enthought.com>
2016-07-13 11:18:06 +0100

    Copy over the QueueWithCallback from the BackgroundIterationController.

--- traits_futures/job_controller.py
+++ traits_futures/job_controller.py
@@ -7,8 +7,39 @@ import Queue as queue
 import concurrent.futures
 
 from traits.api import Any, Dict, HasStrictTraits, Instance, Int
+from traits.trait_notifiers import ui_dispatch
 
 from traits_futures.job import Job
+
+
+class QueueWithCallback(object):
+    """
+    Wrapper around a regular queue that fires a user-supplied callback exactly
+    once for each item placed on the queue.
+    """
+    def __init__(self, queue, callback):
+        # Underlying queue. May be any object that supports the get / put API
+        # of queue.Queue. This trait should be supplied at creation time.
+        self.queue = queue
+
+        # Callback that gets called (with no arguments) every time something is
+        # added to the underlying queue. This is used for example to alert the
+        # UI mainloop that it needs to check the queue at some point in the
+        # future.
+        self.callback = callback
+
+    def put(self, item, block=True, timeout=None):
+        """
+        Put an item into the queue.
+        """
+        self.queue.put(item, block=block, timeout=timeout)
+        self.callback()
+
+    def get(self, block=True, timeout=None):
+        """
+        Remove and return an item from the queue.
+        """
+        return self.queue.get(block=block, timeout=timeout)
 
 
 class JobController(HasStrictTraits):
@@ -21,7 +52,11 @@ class JobController(HasStrictTraits):
     _job_ids = Instance(collections.Iterator)
 
     def __results_queue_default(self):
-        return queue.Queue()
+        # The default message queue ties into a running TraitsUI session.
+        return QueueWithCallback(
+            queue=queue.Queue(),
+            callback=lambda: ui_dispatch(self._process_message),
+        )
 
     def __job_ids_default(self):
         return itertools.count()
@@ -51,6 +86,3 @@ class JobController(HasStrictTraits):
         done = job.process_message(msg[1:])
         if done:
             self._current_jobs.pop(job_id)
-
-    def _remove_job(self, job_id):
-        self._current_jobs.pop(job_id)
