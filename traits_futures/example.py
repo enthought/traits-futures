80cf3b470fe5e4125b58f063955c9bf550787fd1
Mark Dickinson <mdickinson@enthought.com>
2018-07-07 15:45:19 +0100

    Extend example.

--- traits_futures/example.py
+++ traits_futures/example.py
@@ -1,9 +1,14 @@
+from __future__ import division
+
 import random
 import time
 
 import concurrent.futures
+from six.moves import range
 
-from traits.api import Button, HasStrictTraits, Instance, List, Property, Range
+from traits.api import (
+    Bool, Button, HasStrictTraits, Instance, Int, List,
+    on_trait_change, Property, Range, Str)
 from traitsui.api import HGroup, Item, TabularEditor, UItem, VGroup, View
 from traitsui.tabular_adapter import TabularAdapter
 
@@ -17,6 +22,10 @@ from traits_futures.api import (
     FAILED,
     SUCCEEDED,
     WAITING,
+)
+from traits_futures.iteration import (
+    background_iteration,
+    IterationFuture,
 )
 
 
@@ -35,6 +44,21 @@ def slow_square(n, timeout=10.0):
     else:
         time.sleep(sleep_time)
         return n*n
+
+
+def pi_approximations(chunk_size=10**7):
+    """
+    Iterator generating successive approximations to pi,
+    computed via Monte Carlo method.
+    """
+    inside_count = sample_count = 0
+    while True:
+        for _ in range(chunk_size):
+            x = random.uniform(0, 1.0)
+            y = random.uniform(0, 1.0)
+            sample_count += 1
+            inside_count += x * x + y * y <= 1.0
+        yield sample_count, 4.0 * inside_count / sample_count
 
 
 class JobTabularAdapter(TabularAdapter):
@@ -65,8 +89,7 @@ class JobTabularAdapter(TabularAdapter):
         if job.exception is None:
             return "None"
         else:
-            exc_type, exc_message, exc_traceback = job.exception
-            return exc_message
+            return job.exception[0]
 
 
 class SquaringHelper(HasStrictTraits):
@@ -91,6 +114,50 @@ class SquaringHelper(HasStrictTraits):
     #: Value that we'll square.
     input = Range(low=0, high=100)
 
+    #: Start pi calculation.
+    calculate_pi = Button
+
+    #: Cancel pi calculation
+    cancel_pi = Button
+
+    #: Future for pi calculation.
+    pi_future = Instance(IterationFuture)
+
+    #: State of pi calculation.
+    pi_state = Property(Str, depends_on='pi_future.state')
+
+    #: Current approximation, as a formatted string.
+    pi_approximation = Str()
+
+    #: Number of points used in current approximation
+    pi_count = Int()
+
+    can_cancel = Property(Bool, depends_on='pi_future.state')
+
+    can_calculate = Property(Bool, depends_on='pi_future.state')
+
+    def _get_can_cancel(self):
+        return self.pi_future is not None and self.pi_future.cancellable
+
+    def _get_can_calculate(self):
+        return self.pi_future is None or self.pi_future.completed
+
+    def _get_pi_state(self):
+        return "None" if self.pi_future is None else self.pi_future.state
+
+    def _calculate_pi_fired(self):
+        pi_iteration = background_iteration(pi_approximations)
+        self.pi_future = self.job_controller.submit(pi_iteration)
+
+    def _cancel_pi_fired(self):
+        self.pi_future.cancel()
+
+    @on_trait_change('pi_future:result')
+    def _update_pi_approximation(self, new):
+        count, approximation = new
+        self.pi_count = count
+        self.pi_approximation = format(approximation, ".6f")
+
     def _calculate_fired(self):
         job = background_job(slow_square, self.input)
         job_handle = self.job_controller.submit(job)
@@ -111,19 +178,28 @@ class SquaringHelper(HasStrictTraits):
 
     def default_traits_view(self):
         return View(
-            HGroup(
-                VGroup(
-                    Item('input'),
-                    UItem('calculate'),
-                    UItem('cancel_all'),
-                    UItem('clear_completed'),
+            VGroup(
+                HGroup(
+                    Item('calculate_pi', enabled_when='can_calculate'),
+                    Item('cancel_pi', enabled_when='can_cancel'),
+                    Item('pi_state', style="readonly"),
+                    Item('pi_approximation', style="readonly"),
+                    Item('pi_count', style="readonly"),
                 ),
-                VGroup(
-                    UItem(
-                        'current_jobs',
-                        editor=TabularEditor(
-                            adapter=JobTabularAdapter(),
-                            auto_update=True,
+                HGroup(
+                    VGroup(
+                        Item('input'),
+                        UItem('calculate'),
+                        UItem('cancel_all'),
+                        UItem('clear_completed'),
+                    ),
+                    VGroup(
+                        UItem(
+                            'current_jobs',
+                            editor=TabularEditor(
+                                adapter=JobTabularAdapter(),
+                                auto_update=True,
+                            ),
                         ),
                     ),
                 ),
