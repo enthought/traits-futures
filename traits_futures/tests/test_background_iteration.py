e185fa8a9203fdb3322299f47661b6fc49f038ab
Mark Dickinson <mdickinson@enthought.com>
2018-07-25 19:10:18 +0100

    Make sure we're using Python 3 division semantics.

--- traits_futures/tests/test_background_iteration.py
+++ traits_futures/tests/test_background_iteration.py
@@ -1,7 +1,8 @@
 """
 Tests for the background iteration functionality.
 """
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
 
 import contextlib
 import threading
