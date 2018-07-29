70d8d563154bee399947591d711ca68e1e8931b5
Mark Dickinson <mdickinson@enthought.com>
2018-07-29 12:58:33 +0100

    Add version module and tests.

---
+++ traits_futures/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from traits_futures.version import version as __version__
---
+++ traits_futures/tests/test_version.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import re
+import unittest
+
+import six
+
+import traits_futures.version
+
+# Regex matching a version number of one of the two forms
+#
+#   <nnn>.<nnn>.<nnn>
+#
+# or
+#
+#   <nnn>.<nnn>.<nnn>.dev<nnn>
+#
+# We may want to relax the format in the future for alphas, betas and the like.
+
+VERSION_MATCHER = re.compile(r"\A\d+\.\d+.\d+(?:\.dev\d+)?\Z")
+
+
+class TestVersion(unittest.TestCase):
+    def test_version_string(self):
+        version = traits_futures.version.version
+        self.assertIsInstance(version, six.text_type)
+        match = VERSION_MATCHER.match(version)
+        self.assertIsNotNone(
+            match, msg="{!r} appears to be an invalid version".format(version))
+
+    def test_top_level_package_version(self):
+        self.assertEqual(
+            traits_futures.__version__, traits_futures.version.version)
