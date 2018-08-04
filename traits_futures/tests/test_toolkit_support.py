4e439e850ec86eaae7e721a139802b6a021ad5ea
Mark Dickinson <mdickinson@enthought.com>
2018-08-04 20:38:35 +0100

    Use real entry points instead of Mocks.

--- traits_futures/tests/test_toolkit_support.py
+++ traits_futures/tests/test_toolkit_support.py
@@ -15,10 +15,27 @@ from traits_futures.toolkit_support import (
 )
 
 
+class DummyMessageRouter(HasTraits):
+    pass
+
+
+class AnotherDummyMessageRouter(HasTraits):
+    pass
+
+
+def entry_point(cls, toolkit):
+    """
+    Temporary entry point for one of the dummy classes above.
+    """
+    dist = pkg_resources.get_distribution('traits_futures')
+    return pkg_resources.EntryPoint.parse(
+        '{} = {}:{}'.format(toolkit, __name__, cls.__name__), dist)
+
+
 class TestToolkitSupport(unittest.TestCase):
     def test_message_router_class(self):
-        class_ = message_router_class()
-        message_router = class_()
+        router_class = message_router_class()
+        message_router = router_class()
         self.assertIsInstance(message_router, HasTraits)
 
     def test_message_router_class_bad_toolkit(self):
@@ -26,18 +43,22 @@ class TestToolkitSupport(unittest.TestCase):
             message_router_class_for_toolkit("bogus")
 
     def test_message_router_class_multiple_toolkits(self):
-        with mock.patch.object(pkg_resources, 'iter_entry_points') as mock_iep:
-            entry_point1 = mock.Mock()
-            entry_point2 = mock.Mock()
-            mock_iep.return_value = [entry_point1, entry_point2]
+        toolkit = "dummy"
 
-            toolkit = "dummy"
+        with mock.patch.object(pkg_resources, 'iter_entry_points') as mock_iep:
+            mock_iep.return_value = [
+                entry_point(DummyMessageRouter, toolkit),
+                entry_point(AnotherDummyMessageRouter, toolkit),
+            ]
             with warnings.catch_warnings(record=True) as w:
                 warnings.simplefilter("always", RuntimeWarning)
-                message_router_class_for_toolkit(toolkit)
+                router_class = message_router_class_for_toolkit(toolkit)
 
-            self.assertEqual(len(w), 1)
-            warning_message = str(w[0].message)
-            self.assertIn("Multiple", warning_message)
-            self.assertIn(MESSAGE_ROUTER_GROUP, warning_message)
-            self.assertIn(toolkit, warning_message)
+        self.assertEqual(len(w), 1)
+        warning_message = str(w[0].message)
+        self.assertIn("Multiple", warning_message)
+        self.assertIn(MESSAGE_ROUTER_GROUP, warning_message)
+        self.assertIn(toolkit, warning_message)
+
+        message_router = router_class()
+        self.assertIsInstance(message_router, DummyMessageRouter)
