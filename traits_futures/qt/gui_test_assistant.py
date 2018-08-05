3558f67d23521ab3a2d3c36f05a5fab6fe321515
Mark Dickinson <mdickinson@enthought.com>
2018-08-05 19:29:09 +0100

    Inherit from GuiTestAssistant to provide 'run_until_condition' method.

--- traits_futures/qt/gui_test_assistant.py
+++ traits_futures/qt/gui_test_assistant.py
@@ -1,3 +1,23 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-from pyface.ui.qt4.util.gui_test_assistant import GuiTestAssistant  # noqa: F401,E501
+from pyface.ui.qt4.util.gui_test_assistant import (
+    GuiTestAssistant as PyFaceGuiTestAssistant)
+from traits.testing.unittest_tools import _TraitsChangeCollector as \
+    TraitsChangeCollector
+
+
+class GuiTestAssistant(PyFaceGuiTestAssistant):
+    def run_until_condition(self, object, trait, condition, timeout=10.0):
+        """
+        Run the event loop until the given condition holds true. The condition
+        is re-evaluated whenever object.trait changes, and takes the object
+        as a parameter.
+        """
+        collector = TraitsChangeCollector(obj=object, trait=trait)
+
+        collector.start_collecting()
+        try:
+            self.event_loop_helper.event_loop_until_condition(
+                lambda: condition(object), timeout=timeout)
+        finally:
+            collector.stop_collecting()
