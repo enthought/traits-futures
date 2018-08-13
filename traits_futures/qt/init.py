67167154e93d82fd5bf6929e01d0729cdf677bdd
Mark Dickinson <dickinsm@gmail.com>
2018-08-13 20:45:04 +0100

    Force an ImportError from the Qt toolkit if Qt is not installed.

--- traits_futures/qt/init.py
+++ traits_futures/qt/init.py
@@ -4,6 +4,8 @@ Entry point for finding toolkit-specific classes.
 from __future__ import absolute_import, print_function, unicode_literals
 
 from pyface.base_toolkit import Toolkit
+# Force an ImportError if Qt is not installed.
+from pyface.qt import QtCore  # noqa: F401
 
 #: The toolkit object used to find toolkit-specific reources.
 toolkit_object = Toolkit("traits_futures", "qt", "traits_futures.qt")
