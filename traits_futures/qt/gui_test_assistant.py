fe3cb016a4b291a42e7d15dafcb9c8d0f0bdb843
Mark Dickinson <dickinsm@gmail.com>
2018-08-13 14:59:36 +0100

    Remove unneeded bits and pieces from GuiTestAssistant implementation.

--- traits_futures/qt/gui_test_assistant.py
+++ traits_futures/qt/gui_test_assistant.py
@@ -1,23 +1,17 @@
+"""
+Test support, providing the ability to run the event loop from tests.
+"""
+
 from __future__ import absolute_import, print_function, unicode_literals
 
 from pyface.ui.qt4.util.gui_test_assistant import (
     GuiTestAssistant as PyFaceGuiTestAssistant)
-from traits.testing.unittest_tools import _TraitsChangeCollector as \
-    TraitsChangeCollector
 
 
 class GuiTestAssistant(PyFaceGuiTestAssistant):
     def run_until(self, object, trait, condition, timeout=10.0):
         """
-        Run the event loop until the given condition holds true. The condition
-        is re-evaluated whenever object.trait changes, and takes the object
-        as a parameter.
+        Run the event loop until the given condition holds true.
         """
-        collector = TraitsChangeCollector(obj=object, trait=trait)
-
-        collector.start_collecting()
-        try:
-            self.event_loop_helper.event_loop_until_condition(
-                lambda: condition(object), timeout=timeout)
-        finally:
-            collector.stop_collecting()
+        self.event_loop_helper.event_loop_until_condition(
+            lambda: condition(object), timeout=timeout)
