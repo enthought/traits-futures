a2abc461f4884ac9623238dc9c1af3d7fe57d7b1
Mark Dickinson <mdickinson@enthought.com>
2020-07-10 19:23:49 +0100

    Clean up IFuture

--- traits_futures/i_future.py
+++ traits_futures/i_future.py
@@ -17,19 +17,20 @@ class IFuture(ABCHasStrictTraits):
     Interface for futures returned by the executor.
     """
 
-    #: The known state of the background task. One of the constants
-    #: ``EXECUTING``, ``CANCELLING``, ``CANCELLED``, or ``COMPLETED``.
+    #: The known state of the background task. One of the six constants
+    #: ``WAITING``, ``EXECUTING``, ``COMPLETED``, ``FAILED``, ``CANCELLING``
+    #: or ``CANCELLED``.
     state = FutureState
 
     #: True if this task can be cancelled, else False. A task can be
-    #: cancelled only if ``state`` is ``EXECUTING``. The observed value
-    #: of this trait should always be consistent with ``state``.
+    #: cancelled only if ``state`` is ``WAITING`` or ``EXECUTING``. The
+    #: observed value of this trait will always be consistent with ``state``.
     cancellable = Property(Bool())
 
-    #: True if the task state is either ``CANCELLED`` or ``COMPLETED``.
-    #: The observed value of this trait should always be consistent
-    #: with ``state``. It's safe to listen to this trait for changes: it
-    #: will always fire exactly once.
+    #: True if the task has finished and no further state changes will occur.
+    #: The value of ``done`` is ``True`` if and only if the ``state`` attribute
+    #: is one of ``COMPLETED``, ``FAILED``, or ``CANCELLED``. It's safe to
+    #: listen to this trait for changes: it will always fire exactly once.
     done = Property(Bool())
 
     @abc.abstractmethod
@@ -37,13 +38,35 @@ class IFuture(ABCHasStrictTraits):
         """
         Request cancellation of the background task.
 
-        A task in EXECUTING state will immediately be moved to CANCELLING
-        state. If the task is not in EXECUTING state, this function will
-        raise ``RuntimeError``.
+        A task in WAITING or EXECUTING state will immediately be moved to
+        CANCELLING state. If the task is not in WAITING or EXECUTING state,
+        this function will raise ``RuntimeError``.
 
         Raises
         ------
         RuntimeError
             If the task has already completed, or cancellation has already
             been requested.
+        """
+
+    @property
+    @abc.abstractmethod
+    def result(self):
+        """
+        Result of the background task.
+
+        This attribute is only available if the state of the future is
+        COMPLETED. If the future has not reached the COMPLETED state, any
+        attempt to access this attribute will give AttributeError.
+        """
+
+    @property
+    @abc.abstractmethod
+    def exception(self):
+        """
+        Exception raised by the background task.
+
+        This attribute is only available if the state of the future is
+        FAILED. If the future has not reached the FAILED state, any
+        attempt to access this attribute will give AttributeError.
         """
