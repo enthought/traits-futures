1cb1ce390046600f87fe0bf3fd1e2da26b8fa369
Mark Dickinson <mdickinson@enthought.com>
2020-11-26 12:44:38 +0000

    Add wx toolkit support (#246)
    * Add ci support for wxPython installation
    
    * Revert unrelated change
    
    * Refactor to remove duplication
    
    * More refactoring and cleanup
    
    * Add wx toolkit support
    
    * Fix ci install of wxPython to work independently of mode
    
    * Enable faulthandler when running tests
    
    * FIX: use the given timeout rather than the default timeout for the timer
    
    * Restrict wxPython to <4.1 to see if that solves the segfault
    
    * Add quotes around 'wxPython<4.1'
    
    * Add spaces around the '<'
    
    * Add some extra packages on Linux to see  if it solves the segfault
    
    * Add comments to explain dependencies in .travis.yml
    
    * Remove faulthandler
    
    * Add version number
    
    * Remove unnecessary(?) wxPython dependencies
    
    * Add note about why we're creating our own timer class
    
    * Pacify the flake8 check
    
    * Remove event_id argument to _PingEvent constructor
    
    * Also get rid of the expectedIDs argument to PyEventBinder. It's not clear what it does, and we don't seem to need it.
    
    * Clarify _PING_EVENT_TYPE docstring
    
    * Remove doc note about lack of wxPython support

--- traits_futures/tests/test_gui_test_assistant.py
+++ traits_futures/tests/test_gui_test_assistant.py
@@ -45,6 +45,8 @@ class TestGuiTestAssistant(GuiTestAssistant, unittest.TestCase):
     def test_run_until_timeout(self):
         # Trait never fired, condition never true.
         dummy = Dummy()
+
+        start_time = time.monotonic()
         with self.assertRaises(RuntimeError):
             self.run_until(
                 dummy,
@@ -52,11 +54,18 @@ class TestGuiTestAssistant(GuiTestAssistant, unittest.TestCase):
                 condition=lambda object: False,
                 timeout=0.1,
             )
+        actual_timeout = time.monotonic() - start_time
+        # We're being ultra-conservative here; on a typical system, the actual
+        # time taken to fail shouldn't be much more than the 0.1 timeout. But
+        # at least this test catches the bug of using the default timeout of 10
+        # seconds.
+        self.assertLess(actual_timeout, 1.0)
 
     def test_run_until_timeout_trait_fired(self):
         # Trait fired, but condition still never true.
         executor = TraitsExecutor()
         future = submit_call(executor, int, "111")
+        start_time = time.monotonic()
         with self.assertRaises(RuntimeError):
             self.run_until(
                 future,
@@ -64,6 +73,7 @@ class TestGuiTestAssistant(GuiTestAssistant, unittest.TestCase):
                 condition=lambda future: future.state == CANCELLED,
                 timeout=0.1,
             )
+        actual_timeout = time.monotonic() - start_time
 
         executor.stop()
         self.run_until(
@@ -71,16 +81,22 @@ class TestGuiTestAssistant(GuiTestAssistant, unittest.TestCase):
             "stopped",
             condition=lambda executor: executor.stopped,
         )
+        self.assertLess(actual_timeout, 1.0)
 
     def test_run_until_timeout_with_true_condition(self):
         # Trait never fired, but condition true anyway.
         dummy = Dummy()
+        start_time = time.monotonic()
         self.run_until(
             dummy,
             "never_fired",
             condition=lambda object: True,
             timeout=10.0,
         )
+        actual_timeout = time.monotonic() - start_time
+        # The actual time take for the run_until call to return should
+        # be close to zero.
+        self.assertLess(actual_timeout, 1.0)
 
     def test_run_until_success(self):
         # Trait fired, condition starts false but becomes true.
