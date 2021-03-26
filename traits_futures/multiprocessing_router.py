317296d4eeb2a7ab8734f99e9e982a6b3954723f
Mark Dickinson <mdickinson@enthought.com>
2021-03-26 10:41:37 +0000

    Add new routing classes for multithreading and multiprocessing
    This commit adds the IMessageRouter, IMessageSender and IMessageReceiver
    interfaces, and implementations of those interfaces intended for
    background tasks running (a) in a background thread, or (b) in a
    separate child process.
    
    The new classes are similar in role to the old ones. A key
    difference is that the message router is no longer responsible for
    determining when to dispose of a receiver; instead, the code
    that requests the (sender, receiver) pair is determining when
    the receiver is no longer needed.

---
+++ traits_futures/multiprocessing_router.py
@@ -0,0 +1,394 @@
+# (C) Copyright 2018-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+"""
+Implementations of the IMessageSender, IMessageReceiver and IMessageRouter
+interfaces for tasks executed in a background process.
+"""
+
+import collections.abc
+import itertools
+import logging
+import multiprocessing.managers
+import queue
+import threading
+
+from traits.api import (
+    Any,
+    Bool,
+    Dict,
+    Event,
+    HasStrictTraits,
+    Instance,
+    Int,
+    provides,
+)
+
+from traits_futures.i_message_router import (
+    IMessageRouter,
+    IMessageSender,
+    IMessageReceiver,
+)
+from traits_futures.toolkit_support import toolkit
+
+logger = logging.getLogger(__name__)
+
+Pingee = toolkit("pinger:Pingee")
+Pinger = toolkit("pinger:Pinger")
+
+
+#: Internal states for the sender. The sender starts in the _INITIAL state,
+#: moves to the _OPEN state when 'start' is called, and from _OPEN to _CLOSED
+#: when 'stop' is called. Messages can only be sent with the 'send' method
+#: while the sender is in _OPEN state.
+_INITIAL = "initial"
+_OPEN = "open"
+_CLOSED = "closed"
+
+
+@IMessageSender.register
+class MultiprocessingSender:
+    """
+    Object allowing the worker to send messages.
+
+    This class will be instantiated in the main thread, and the instance passed
+    to the worker process to allow the worker to communicate back to the main
+    thread.
+
+    Parameters
+    ----------
+    connection_id : int
+        Id of the matching receiver; used for message routing.
+    message_queue : multiprocessing.Queue
+        Process-safe queue for passing messages to the foreground.
+    """
+
+    def __init__(self, connection_id, message_queue):
+        self.connection_id = connection_id
+        self.message_queue = message_queue
+        self._state = _INITIAL
+
+    def start(self):
+        """
+        Do any setup necessary to prepare for sending messages.
+
+        This method must be called before any messages can be sent
+        using the ``send`` method.
+
+        Not thread-safe. The 'start', 'send' and 'stop' methods should
+        all be called from the same thread.
+
+        Raises
+        ------
+        RuntimeError
+            If the sender has previously been started.
+        """
+        if self._state != _INITIAL:
+            raise RuntimeError(
+                f"Sender already started: state is {self._state}"
+            )
+
+        self._state = _OPEN
+
+    def send(self, message):
+        """
+        Send a message to the router.
+
+        Parameters
+        ----------
+        message : object
+            Typically this will be immutable, small, and pickleable.
+
+        Not thread-safe. The 'start', 'send' and 'stop' methods should
+        all be called from the same thread.
+
+        Raises
+        ------
+        RuntimeError
+            If the sender has not been started, or has already been stopped.
+        """
+        if self._state != _OPEN:
+            raise RuntimeError(
+                "Sender must be in OPEN state to send messages: "
+                f"state is {self._state}"
+            )
+
+        self.message_queue.put((self.connection_id, message))
+
+    def stop(self):
+        """
+        Do any teardown.
+
+        After this method has been called, no more messages can be sent.
+
+        Not thread-safe. The 'start', 'send' and 'stop' methods should
+        all be called from the same thread.
+
+        Raises
+        ------
+        RuntimeError
+            If the sender has not been started, or has already been stopped.
+        """
+        if self._state != _OPEN:
+            raise RuntimeError(
+                "Sender not started, or already stopped: "
+                f"state is {self._state}"
+            )
+
+        self._state = _CLOSED
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *exc_info):
+        self.stop()
+
+
+@provides(IMessageReceiver)
+class MultiprocessingReceiver(HasStrictTraits):
+    """
+    Implementation of the IMessageReceiver interface for the case where the
+    sender will be in a background process.
+    """
+
+    #: Event fired when a message is received from the paired sender.
+    message = Event(Any())
+
+    #: Connection id, matching that of the paired sender.
+    connection_id = Int()
+
+
+@provides(IMessageRouter)
+class MultiprocessingRouter(HasStrictTraits):
+    """
+    Implementation of the IMessageRouter interface for the case where the
+    sender will be in a background process.
+    """
+
+    def start(self):
+        """
+        Start routing messages.
+
+        This method must be called before any call to ``pipe`` or
+        ``close_pipe`` can be made.
+
+        Not thread-safe. Must always be called in the main thread.
+
+        Raises
+        ------
+        RuntimeError
+            If the router has already been started.
+        """
+        if self._running:
+            raise RuntimeError("Router is already running")
+
+        self._manager = multiprocessing.Manager()
+        self._local_message_queue = queue.Queue()
+        self._process_message_queue = self._manager.Queue()
+
+        self._pingee = Pingee(on_ping=self._route_message)
+        self._pingee.connect()
+
+        self._monitor_thread = threading.Thread(
+            target=monitor_queue,
+            args=(
+                self._process_message_queue,
+                self._local_message_queue,
+                self._pingee,
+            ),
+        )
+        self._monitor_thread.start()
+
+        self._running = True
+
+    def stop(self):
+        """
+        Stop routing messages.
+
+        This method should be called in the main thread after all pipes
+        are finished with. Calls to ``pipe`` or ``close_pipe`` are
+        not permitted after this method has been called.
+
+        Logs a warning if there are unclosed pipes.
+
+        Not thread safe. Must always be called in the main thread.
+
+        Raises
+        ------
+        RuntimeError
+            If the router was not already running.
+        """
+        if not self._running:
+            raise RuntimeError("Router is not running")
+
+        if self._receivers:
+            logger.warning("there are unclosed pipes")
+
+        # Shut everything down in reverse order.
+        # First the monitor thread.
+        self._process_message_queue.put(None)
+        self._monitor_thread.join()
+        self._monitor_thread = None
+
+        # No shutdown required for the process_message_queue: it's enough
+        # to shut down the manager.
+        self._manager.shutdown()
+        self._manager = None
+        self._process_message_queue = None
+
+        self._pingee.disconnect()
+        self._pingee = None
+
+        self._local_message_queue = None
+
+        self._running = False
+
+    def pipe(self):
+        """
+        Create a (sender, receiver) pair for sending and receiving messages.
+
+        The sender will be passed to the background task and used to send
+        messages, while the receiver remains in the foreground.
+
+        Not thread safe. Must always be called in the main thread.
+
+        Returns
+        -------
+        sender : MultiprocessingSender
+            Object to be passed to the background task.
+        receiver : MultiprocessingReceiver
+            Object kept in the foreground, which reacts to messages.
+
+        Raises
+        ------
+        RuntimeError
+            If the router is not currently running.
+        """
+        if not self._running:
+            raise RuntimeError("Router is not running.")
+
+        connection_id = next(self._connection_ids)
+
+        sender = MultiprocessingSender(
+            connection_id=connection_id,
+            message_queue=self._process_message_queue,
+        )
+        receiver = MultiprocessingReceiver(connection_id=connection_id)
+        self._receivers[connection_id] = receiver
+        return sender, receiver
+
+    def close_pipe(self, receiver):
+        """
+        Close the receiver end of a pipe produced by ``pipe``.
+
+        Removes the receiver from the routing table, so that no new messages
+        can reach that receiver.
+
+        Not thread safe. Must always be called in the main thread.
+
+        Parameters
+        ----------
+        receiver : MultiprocessingReceiver
+            Receiver half of the pair returned by the ``pipe`` method.
+
+        Raises
+        ------
+        RuntimeError
+            If the router is not currently running.
+        """
+        if not self._running:
+            raise RuntimeError("Router is not running.")
+
+        connection_id = receiver.connection_id
+        self._receivers.pop(connection_id)
+
+    # Private traits ##########################################################
+
+    #: Queue receiving messages from child processes.
+    _process_message_queue = Any()
+
+    #: Local queue for messages to the UI thread.
+    _local_message_queue = Instance(queue.Queue)
+
+    #: Thread transferring messages from the process queue to the local queue.
+    _monitor_thread = Any()
+
+    #: Source of new connection ids.
+    _connection_ids = Instance(collections.abc.Iterator)
+
+    #: Receivers, keyed by connection_id.
+    _receivers = Dict(Int(), Instance(MultiprocessingReceiver))
+
+    #: Receiver for the "message_sent" signal.
+    _pingee = Instance(Pingee)
+
+    #: Manager, used to create message queues.
+    _manager = Instance(multiprocessing.managers.BaseManager)
+
+    #: Router status: True if running, False if stopped.
+    _running = Bool(False)
+
+    # Private methods #########################################################
+
+    def _route_message(self):
+        connection_id, message = self._local_message_queue.get()
+        try:
+            receiver = self._receivers[connection_id]
+        except KeyError:
+            logger.warning(
+                f"No receiver for message with connection_id {connection_id}. "
+                "Message discarded."
+            )
+        else:
+            receiver.message = message
+
+    def __connection_ids_default(self):
+        return itertools.count()
+
+
+def monitor_queue(process_queue, local_queue, pingee):
+    """
+    Move incoming child process messages to the local queue.
+
+    Monitors the process queue for incoming messages, and transfers
+    those messages to the local queue, while also requesting that
+    the event loop eventually process that message.
+
+    To stop the thread, put ``None`` onto the process_queue.
+
+    Parameters
+    ----------
+    process_queue : multiprocessing.Queue
+        Queue to listen to for messages.
+    local_queue : queue.Queue
+        Queue to transfer those messages to.
+    pingee : Pingee
+        Recipient for pings, used to notify the GUI thread that there's
+        a message pending.
+
+    """
+    pinger = Pinger(pingee=pingee)
+    pinger.connect()
+    try:
+        while True:
+            try:
+                message = process_queue.get(block=True, timeout=1.0)
+            except queue.Empty:
+                continue
+            if message is None:
+                break
+            local_queue.put(message)
+            # Avoid hanging onto a reference to the message until the next
+            # queue element arrives.
+            del message
+            pinger.ping()
+    finally:
+        pinger.disconnect()
---
+++ traits_futures/tests/i_message_router_tests.py
@@ -0,0 +1,386 @@
+# (C) Copyright 2018-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+"""
+Common tests for testing implementations of the IMessageRouter interface.
+"""
+
+import concurrent.futures
+import contextlib
+import logging
+import threading
+
+from traits.api import (
+    Any,
+    HasStrictTraits,
+    Instance,
+    List,
+    on_trait_change,
+    Str,
+)
+
+from traits_futures.i_message_router import IMessageReceiver
+from traits_futures.i_message_router import IMessageRouter
+
+
+#: Safety timeout, in seconds, for blocking operations, to prevent
+#: the test suite from blocking indefinitely if something goes wrong.
+SAFETY_TIMEOUT = 10.0
+
+
+def send_messages(sender, messages):
+    """
+    Send a sequence of messages using the given sender.
+
+    Parameters
+    ----------
+    sender : IMessageSender
+        The sender to use to send messages.
+    messages : list
+        List of objects to send.
+    """
+    sender.start()
+    for message in messages:
+        sender.send(message)
+    sender.stop()
+
+
+class ReceiverListener(HasStrictTraits):
+    """
+    Listener for a receiver, recording message received.
+    """
+
+    #: The receiver that we're listening to.
+    receiver = Instance(IMessageReceiver)
+
+    #: Received messages
+    messages = List(Any())
+
+    @on_trait_change("receiver:message")
+    def _record_message(self, message):
+        self.messages.append(message)
+
+
+class CapturingHandler(logging.Handler):
+    """
+    Simple logging handler that just emits records to a list.
+    """
+
+    def __init__(self, level=0):
+        logging.Handler.__init__(self)
+        self.watcher = LoggingWatcher()
+
+    def flush(self):
+        pass
+
+    def emit(self, record):
+        self.watcher.records.append(record)
+        msg = self.format(record)
+        self.watcher.output.append(msg)
+
+
+class LoggingWatcher(HasStrictTraits):
+    """
+    Traited logging watcher that records raw records and formatted messages.
+    """
+
+    #: Logged records.
+    records = List(Instance(logging.LogRecord))
+
+    #: Formatted logged meesages.
+    output = List(Str())
+
+
+class IMessageRouterTests:
+    """
+    Test mix-in for testing implementations of the IMessageRouter interface.
+
+    Should be used in conjunction with the GuiTestAssistant.
+    """
+
+    #: Factory providing the routers under test.
+    # Override this in implementations. It should be a zero-argument
+    # callable that produces the appropriate IMessageRouter instances
+    # when called.
+    router_factory = IMessageRouter
+
+    #: Factory providing worker pools for the tests.
+    # Override this in implementations. It should be a zero-argument
+    # callback that produces something that follows the concurrent.futures
+    # Executor API.
+    executor_factory = concurrent.futures.Executor
+
+    def test_send_and_receive(self):
+        with self.executor_factory() as worker_pool:
+            with self.started_router() as router:
+                # Given
+                sender, receiver = router.pipe()
+                listener = ReceiverListener(receiver=receiver)
+                messages = ["Just testing", 314]
+
+                # When
+                worker_pool.submit(send_messages, sender, messages)
+
+                # Then
+                self.assertEventuallyReceives(listener, messages)
+
+                # Clean up
+                router.close_pipe(receiver)
+
+    def test_send_and_receive_multiple_senders(self):
+        # Variant of test_send_and_receive for multiple independent
+        # message streams.
+
+        # Labels used to identify tasks.
+        tasks = range(64)
+
+        with self.executor_factory() as worker_pool:
+            with self.started_router() as router:
+                # Senders, receivers, listeners and messages, keyed by task.
+                sender, receiver, listener, messages = {}, {}, {}, {}
+
+                # Create one pipe per task.
+                for task in tasks:
+                    sender[task], receiver[task] = router.pipe()
+                    listener[task] = ReceiverListener(receiver=receiver[task])
+                    messages[task] = [(task, message) for message in range(64)]
+
+                # Start the jobs
+                for task in tasks:
+                    worker_pool.submit(
+                        send_messages, sender[task], messages[task]
+                    )
+
+                # Check the results.
+                for task in tasks:
+                    self.assertEventuallyReceives(
+                        listener[task], messages[task]
+                    )
+
+                # Clean up.
+                for task in tasks:
+                    router.close_pipe(receiver[task])
+
+    def test_send_and_receive_main_thread(self):
+        # Variant of send_and_receive where we send on the main thread,
+        # without using a worker. This should still work.
+        with self.started_router() as router:
+            # Given
+            sender, receiver = router.pipe()
+            listener = ReceiverListener(receiver=receiver)
+            messages = ["Just testing", 314]
+
+            # When
+            send_messages(sender, messages)
+
+            # Then
+            self.assertEventuallyReceives(listener, messages)
+
+            # Cleanup
+            router.close_pipe(receiver)
+
+    def test_start_already_started_router(self):
+        with self.started_router() as router:
+            with self.assertRaises(RuntimeError):
+                router.start()
+
+    def test_stop_unstarted_router(self):
+        router = self.router_factory()
+        with self.assertRaises(RuntimeError):
+            router.stop()
+
+    def test_pipe_for_unstarted_router(self):
+        router = self.router_factory()
+        with self.assertRaises(RuntimeError):
+            router.pipe()
+
+    def test_pipe_for_stopped_router(self):
+        with self.started_router() as router:
+            pass
+        with self.assertRaises(RuntimeError):
+            router.pipe()
+
+    def test_close_pipe_for_stopped_router(self):
+        with self.assertLogs("traits_futures", level="WARNING"):
+            with self.started_router() as router:
+                _, receiver = router.pipe()
+
+        with self.assertRaises(RuntimeError):
+            router.close_pipe(receiver)
+
+    def test_unclosed_pipe(self):
+        # Stopping the router with unclosed pipes
+        with self.assertLogs("traits_futures", level="WARNING") as cm:
+            with self.started_router() as router:
+                router.pipe()
+
+        self.assertTrue(
+            any(
+                "there are unclosed pipes" in log_message
+                for log_message in cm.output
+            ),
+            msg="Expected log message not found",
+        )
+
+    def test_threads_cleaned_up(self):
+        threads_before = threading.enumerate()
+        with self.started_router():
+            pass
+        threads_after = threading.enumerate()
+        self.assertEqual(len(threads_after), len(threads_before))
+
+    def test_send_after_close(self):
+        with self.started_router() as router:
+            sender, receiver = router.pipe()
+            router.close_pipe(receiver)
+
+            # There should be no problem with sending. But eventually the
+            # router will receive the message and fail to find a receiver.
+            # At that point it should log.
+            with self.assertEventuallyLogs(
+                "traits_futures", level="WARNING"
+            ) as cm:
+                sender.start()
+                sender.send("some message")
+                sender.stop()
+
+            self.assertTrue(
+                any(
+                    "No receiver for message" in log_message
+                    for log_message in cm.output
+                ),
+                msg="Expected log message not found",
+            )
+
+    def test_sender_send_without_start(self):
+        with self.started_router() as router:
+            with self.get_sender(router) as sender:
+                with self.assertRaises(RuntimeError):
+                    sender.send("Some message")
+
+    def test_sender_start_twice(self):
+        with self.started_router() as router:
+            with self.get_sender(router) as sender:
+                sender.start()
+                with self.assertRaises(RuntimeError):
+                    sender.start()
+                sender.stop()
+
+    def test_sender_stop_before_start(self):
+        with self.started_router() as router:
+            with self.get_sender(router) as sender:
+                with self.assertRaises(RuntimeError):
+                    sender.stop()
+
+    def test_sender_stop_twice(self):
+        with self.started_router() as router:
+            with self.get_sender(router) as sender:
+                sender.start()
+                sender.stop()
+                with self.assertRaises(RuntimeError):
+                    sender.stop()
+
+    def test_sender_restart(self):
+        with self.started_router() as router:
+            with self.get_sender(router) as sender:
+                sender.start()
+                sender.stop()
+                with self.assertRaises(RuntimeError):
+                    sender.start()
+
+    @contextlib.contextmanager
+    def get_sender(self, router):
+        """
+        Provide a sender for the given router, and do cleanup afterwards.
+        """
+        sender, receiver = router.pipe()
+        try:
+            yield sender
+        finally:
+            router.close_pipe(receiver)
+
+    # Helper functions and assertions
+
+    @contextlib.contextmanager
+    def started_router(self):
+        """
+        A context manager that yields an already-started router.
+
+        Stops the router on context manager exit.
+        """
+        router = self.router_factory()
+        router.start()
+        try:
+            yield router
+        finally:
+            router.stop()
+
+    def assertEventuallyReceives(
+        self, listener, messages, *, timeout=SAFETY_TIMEOUT
+    ):
+        """
+        Assert that a given listener eventually receives the given messages.
+
+        Runs the event loop until either the expected messages are received,
+        or until timeout.
+        """
+
+        def got_expected_messages(listener):
+            return listener.messages[: len(messages)] == messages
+
+        self.run_until(
+            object=listener,
+            trait="messages_items",
+            condition=got_expected_messages,
+            timeout=timeout,
+        )
+
+    @contextlib.contextmanager
+    def assertEventuallyLogs(
+        self, logger=None, level=logging.INFO, *, timeout=SAFETY_TIMEOUT
+    ):
+        """
+        Assert that we eventually get a log message matching the given pattern.
+
+        Runs the event loop until either the expected log message is received,
+        or until timeout. The API roughly matches that of the standard library
+        assertLogs method, and much of the code is shamelessly adapted from
+        the assertLogs source.
+        """
+
+        # Turn a logger name into the corresponding logger.
+        if not isinstance(logger, logging.Logger):
+            logger = logging.getLogger(logger)
+
+        formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+        handler = CapturingHandler()
+        handler.setFormatter(formatter)
+        watcher = handler.watcher
+
+        old_handlers = logger.handlers[:]
+        old_level = logger.level
+        old_propagate = logger.propagate
+
+        logger.handlers = [handler]
+        logger.setLevel(level)
+        logger.propagate = False
+
+        try:
+            yield watcher
+            self.run_until(
+                object=watcher,
+                trait="records_items",
+                condition=lambda watcher: len(watcher.records) > 0,
+                timeout=timeout,
+            )
+        finally:
+            logger.handlers = old_handlers
+            logger.propagate = old_propagate
+            logger.setLevel(old_level)
