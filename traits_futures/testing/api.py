3abcb1753a0884a31f0a1ffc9e3d121f6072ac11
Mark Dickinson <mdickinson@enthought.com>
2021-04-02 16:36:52 +0100

    Add tests for the entry points

---
+++ traits_futures/testing/api.py
@@ -0,0 +1,34 @@
+# (C) Copyright 2018-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+"""
+Testing helpers and utilities.
+"""
+
+import unittest
+
+try:
+    from pyface.qt import QtCore
+except ImportError:
+    QtCore = None
+requires_qt = unittest.skipIf(QtCore is None, "Qt not available")
+
+
+try:
+    import wx
+except ImportError:
+    wx = None
+requires_wx = unittest.skipIf(wx is None, "wx not available")
+
+
+__all__ = [
+    "requires_qt",
+    "requires_wx",
+]
--- traits_futures/tests/test_toolkit_support.py
+++ traits_futures/tests/test_toolkit_support.py
@@ -10,10 +10,58 @@
 
 import unittest
 
+try:
+    # importlib.metadata only exists from Python 3.8 onwards. For earlier
+    # versions of Python, we use the PyPI package instead.
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+
+from pyface.base_toolkit import Toolkit
+
+# from traits_futures.testing.api import requires_qt, requires_wx
 from traits_futures.toolkit_support import toolkit
+
+#: Name of the setuptools entry_point group for Traits Futures toolkits
+TOOLKIT_ENTRY_POINT_GROUP = "traits_futures.toolkits"
 
 
 class TestToolkitSupport(unittest.TestCase):
+    def test_asyncio_entry_point(self):
+        entry_points = self.toolkit_entry_points("asyncio")
+        self.assertEqual(len(entry_points), 1)
+        entry_point = entry_points[0]
+        toolkit = entry_point.load()
+        self.assertIsInstance(toolkit, Toolkit)
+
+    def test_null_entry_point(self):
+        entry_points = self.toolkit_entry_points("null")
+        self.assertEqual(len(entry_points), 1)
+        entry_point = entry_points[0]
+        toolkit = entry_point.load()
+        self.assertIsInstance(toolkit, Toolkit)
+
+    def test_wx_entry_point(self):
+        entry_points = self.toolkit_entry_points("wx")
+        self.assertEqual(len(entry_points), 1)
+        entry_point = entry_points[0]
+        toolkit = entry_point.load()
+        self.assertIsInstance(toolkit, Toolkit)
+
+    def test_qt4_entry_point(self):
+        entry_points = self.toolkit_entry_points("qt4")
+        self.assertEqual(len(entry_points), 1)
+        entry_point = entry_points[0]
+        toolkit = entry_point.load()
+        self.assertIsInstance(toolkit, Toolkit)
+
+    def test_qt_entry_point(self):
+        entry_points = self.toolkit_entry_points("qt")
+        self.assertEqual(len(entry_points), 1)
+        entry_point = entry_points[0]
+        toolkit = entry_point.load()
+        self.assertIsInstance(toolkit, Toolkit)
+
     def test_gui_test_assistant(self):
         GuiTestAssistant = toolkit("gui_test_assistant:GuiTestAssistant")
         test_assistant = GuiTestAssistant()
@@ -26,3 +74,18 @@ class TestToolkitSupport(unittest.TestCase):
         pingee = Pingee(on_ping=lambda: None)
         pinger = Pinger(pingee=pingee)
         self.assertTrue(hasattr(pinger, "ping"))
+
+    # Helper functions
+
+    def toolkit_entry_points(self, name):
+        """
+        Return all entry points for the "traits_futures.toolkits" toolkit
+        with the given name.
+        """
+        return [
+            entry_point
+            for entry_point in importlib_metadata.entry_points()[
+                TOOLKIT_ENTRY_POINT_GROUP
+            ]
+            if entry_point.name == name
+        ]
