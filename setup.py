689d4620ccb286086bcb52e4e793a8fdcaa9ae71
Mark Dickinson <mdickinson@enthought.com>
2021-04-02 16:09:54 +0100

    Rename traits_futures.null to traits_futures.asyncio

--- setup.py
+++ setup.py
@@ -65,7 +65,8 @@ setup(
     ],
     entry_points={
         "traits_futures.toolkits": [
-            "null = traits_futures.null.init:toolkit_object",
+            "asyncio = traits_futures.asyncio.init:toolkit_object",
+            "null = traits_futures.asyncio.init:toolkit_object",
             "qt4 = traits_futures.qt.init:toolkit_object",
             "qt = traits_futures.qt.init:toolkit_object",
             "wx = traits_futures.wx.init:toolkit_object",
--- traits_futures/asyncio/null/init.py
+++ traits_futures/asyncio/null/init.py
@@ -14,4 +14,4 @@ Entry point for finding toolkit specific classes.
 from pyface.base_toolkit import Toolkit
 
 #: The toolkit object used to find toolkit-specific resources.
-toolkit_object = Toolkit("traits_futures", "null", "traits_futures.null")
+toolkit_object = Toolkit("traits_futures", "asyncio", "traits_futures.asyncio")
