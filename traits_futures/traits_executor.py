c4101c876b048c5352c31e4ff1a817a9b2989461
Mark Dickinson <mdickinson@enthought.com>
2021-04-26 15:44:18 +0100

    Adapt for rename upstream: lazy_context -> ETSContext()

--- traits_futures/traits_executor.py
+++ traits_futures/traits_executor.py
@@ -357,9 +357,9 @@ class TraitsExecutor(HasStrictTraits):
     def __context_default(self):
         # By default, we use multithreading
         from traits_futures.multithreading_context import MultithreadingContext
-        from traits_futures.toolkit_support import lazy_toolkit
+        from traits_futures.ets_context import ETSContext
 
-        context = MultithreadingContext(toolkit=lazy_toolkit)
+        context = MultithreadingContext(toolkit=ETSContext())
         self._own_context = True
         return context
 
