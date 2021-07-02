915ba4f921587319d5c5e7c5829b5cf76adaa5d0
Mark Dickinson <mdickinson@enthought.com>
2021-07-02 13:23:23 +0100

    Set 3.5 as the minimum Sphinx version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,9 @@ import traits_futures.version
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
+# Sphinx versions older than 3.5 run into issues with the autodoc_mock_imports
+# setting below.
+needs_sphinx = '3.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
