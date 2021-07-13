8e0566e3045824cc4e1bd9206384b50cc5a41201
Mark Dickinson <mdickinson@enthought.com>
2021-07-13 12:52:41 +0100

    Style: fix inconsistent string quotes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,16 +100,15 @@ add_function_parentheses = False
 
 # Ignore complaints about references to classes in wx and pyface.qt.QtCore
 nitpick_ignore = [
-    ('py:class', 'PySide2.QtCore.QObject'),
-    ('py:class', 'wx.core.App'),
-    ('py:class', 'wx._core.EvtHandler'),
-    ('py:class', 'wx._core.Timer'),
-
+    ("py:class", "PySide2.QtCore.QObject"),
+    ("py:class", "wx.core.App"),
+    ("py:class", "wx._core.EvtHandler"),
+    ("py:class", "wx._core.Timer"),
     # These two slightly strange class descriptions (note the trailing dot)
     # appear in warnings as a result of the later `autodoc_mock_imports`
     # setting.
-    ('py:class', 'pyface.qt.QtCore.'),
-    ('py:class', 'wx.'),
+    ("py:class", "pyface.qt.QtCore."),
+    ("py:class", "wx."),
 ]
 
 # -- Options for Napoleon extension ---------------------------------------
