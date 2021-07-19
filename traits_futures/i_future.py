8529206326ddd05821be975967e30af633033e0f
Mark Dickinson <mdickinson@enthought.com>
2021-07-19 08:14:40 +0100

    Reflow text

--- traits_futures/i_future.py
+++ traits_futures/i_future.py
@@ -19,9 +19,8 @@ class IFuture(abc.ABC):
     """
     Interface for futures returned by the executor.
 
-    This interface can be used to implement new
-    background task types. It represents the knowledge that the executor needs
-    to interact with futures.
+    This interface can be used to implement new background task types. It
+    represents the knowledge that the executor needs to interact with futures.
     """
 
     @abc.abstractmethod
